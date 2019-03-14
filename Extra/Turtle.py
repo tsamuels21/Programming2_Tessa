27b1c544a5bd051f7137e7eaea2ab7a7673cf22c
tessasamuels <tsamuels@fwparker.org>
2019-03-14 12:51:06 -0500

    3/14/19

---
+++ Extra/Turtle.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+my_turtle = Turtle()
+my_window = Screen()
+
+my_turtle.goto(0, 0)
+# my_turtle.goto(100, 100)
+
+
+my_window.colormode(255)
+for j in range(20):
+    my_turtle.begin_fill()
+    my_turtle.left(20)
+    for i in range(3):
+        my_turtle.fillcolor((255, j * 10, j * 10))
+        my_turtle.right(120)
+        my_turtle.forward(10 * j)
+    my_turtle.end_fill()
+
+my_window.exitonclick()
---
+++ Notes/Exceptions.py
@@ -0,0 +1,7 @@
+# Exceptions    (short topic)
+
+try:
+    val = int(input("Enter a number: "))
+except:
+    print("You entered an invalid number")
+
